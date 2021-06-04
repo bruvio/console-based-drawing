5b9a77132fa782a8a391326675386c900424ade5
bruvio <bruno.viola@protonmail.com>
2021-06-04 05:55:52 +0100

    testing write canvas

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -1,5 +1,5 @@
 import pytest
-from canvas import createCanvas, modifyPixel
+from canvas import createCanvas, modifyPixel, writeCanvas2File
 
 
 @pytest.mark.parametrize("x", [3, 4, 1])
@@ -38,3 +38,23 @@ def test_modifyPixel(x, y, point):
     can = createCanvas(x, y)
     can = modifyPixel(can, point[0], point[1], "f")
     assert can[point[0]][point[1]] == "f"
+
+
+# pytest.mark.usefixtures("canvas")
+
+
+@pytest.mark.parametrize("x", [3, 4, 5])
+@pytest.mark.parametrize("y", [4, 5, 20])
+def test_writeCanvas2File(x, y):
+    canvas = createCanvas(x, y)
+    test_file = "test-output.txt"
+    test_path = "".join(test_file)
+    writeCanvas2File(canvas, test_path)
+
+    with open(test_file, "r") as testoutput:
+        text = testoutput.readlines()
+
+    for i in range(0, len(canvas)):
+        lst = canvas[i]
+        lst = "".join(map(str, lst)) + "\n"
+        assert lst == text[i]
