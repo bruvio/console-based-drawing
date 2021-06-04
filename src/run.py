5f62bf0e2f27b7b157350f20e54387082bf15762
bruvio <bruno.viola@protonmail.com>
2021-06-04 17:33:39 +0100

    debugging selection menuu

--- src/run.py
+++ src/run.py
@@ -1,4 +1,4 @@
-from canvas import createCanvas
+from canvas import createCanvas, writeCanvas2File, fillArea, drawRectangle, drawLine
 
 # Import the modules needed to run the script.
 import sys
@@ -22,7 +22,7 @@ def main_menu():
     print("This is a console based drawing program \n")
     print("do you want to draw a canvas?\n")
     print("1. Yes")
-    print("2. No")
+    # print("2. No")
     print("\nq. Quit")
     choice = input(" >>  ")
     exec_menu(choice)
@@ -45,18 +45,34 @@ def exec_menu(choice):
     return
 
 
-# def exec_menu(choice):
-#     os.system("clear")
-#     ch = choice.lower()
-#     if ch == "":
-#         menu_actions["main_menu"]()
-#     else:
-#         try:
-#             menu_actions[ch]()
-#         except KeyError:
-#             print("Invalid selection, please try again.\n")
-#             menu_actions["main_menu"]()
-#     return
+def exec_drawmenu(choice):
+    os.system("clear")
+    ch = choice.lower()
+    if ch == "":
+        menu_actions["main_menu"]()
+    else:
+        try:
+            exec_draw[ch]()
+        except KeyError:
+            print("Invalid selection, please try again.\n")
+            menu_actions["main_menu"]()
+    return
+
+
+def draw_menu(canvas):
+    os.system("clear")
+    print("Here is your canvas,\n")
+    printCanvas(canvas)
+    print("\n")
+    print("What do you want to do now?")
+    print("\n")
+    print("1. Draw a new canvas")
+    print("2. Draw a line")
+    print("3. Fill the canvas")
+    print("4. Save the canvas to file")
+    print("\nq. Quit")
+    choice = input(" >>  ")
+    exec_drawmenu(choice)
 
 
 # Menu 1
@@ -74,6 +90,7 @@ def menu1():
                     height = int(x[2])
                     canvas = createCanvas(width, height)
                     printCanvas(canvas)
+                    draw_menu(canvas)
                 except Exception:
                     print("Invalid selection, please try again.\n")
                     menu_actions[1]()
@@ -84,14 +101,125 @@ def menu1():
     return
 
 
-# Menu 2
-def menu2():
-    print("Hello Menu 2 !\n")
-    print("9. Back")
-    print("q. Quit")
-    choice = input(" >>  ")
-    exec_menu(choice)
-    return
+def menu2(canvas):
+    print("\n")
+    x = []
+    while len(x) != 5:
+        try:
+            x = list(
+                map(
+                    str,
+                    input("The command to draw a line is L x1 y1 x2 y2: ").split(),
+                )
+            )
+            if (type(x[0]) == str) and (x[0] == "L"):
+                try:
+                    x1 = int(x[1])
+                    y1 = int(x[2])
+                    x2 = int(x[3])
+                    y2 = int(x[4])
+                    canvas = drawLine(canvas, x1, y1, x2, y2)
+                    printCanvas(canvas)
+                    draw_menu()
+                except Exception:
+                    print("Invalid selection, please try again.\n")
+                    draw_menu()
+        except KeyError:
+            print("Invalid selection, please try again.\n")
+            draw_menu()
+
+    return canvas
+
+
+def menu3(canvas):
+    print("\n")
+    x = []
+    while len(x) != 5:
+        try:
+            x = list(
+                map(
+                    str,
+                    input("The command to Draw a rectangle is R x1 y1 x2 y2: ").split(),
+                )
+            )
+            if (type(x[0]) == str) and (x[0] == "R"):
+                try:
+                    x1 = int(x[1])
+                    y1 = int(x[2])
+                    x2 = int(x[3])
+                    y2 = int(x[4])
+                    canvas = drawRectangle(canvas, x1, y1, x2, y2)
+                    printCanvas(canvas)
+                    draw_menu()
+                except Exception:
+                    print("Invalid selection, please try again.\n")
+                    draw_menu()
+        except KeyError:
+            print("Invalid selection, please try again.\n")
+            draw_menu()
+
+    return canvas
+
+
+def menu4(canvas):
+    print("\n")
+    x = []
+    while len(x) != 4:
+        try:
+            x = list(
+                map(
+                    str,
+                    input(
+                        "The command to fill an area containing a point is B x y c: "
+                    ).split(),
+                )
+            )
+            if (type(x[0]) == str) and (x[0] == "L"):
+                try:
+                    x = int(x[1])
+                    y = int(x[2])
+                    colour = x[3]
+
+                    canvas = fillArea(canvas, x, y, colour)
+                    printCanvas(canvas)
+                    draw_menu()
+                except Exception:
+                    print("Invalid selection, please try again.\n")
+                    draw_menu()
+        except KeyError:
+            print("Invalid selection, please try again.\n")
+            draw_menu()
+
+    return canvas
+
+
+def menu5(canvas):
+    print("\n")
+    x = []
+    while len(x) != 2:
+        try:
+            x = list(
+                map(
+                    str,
+                    input(
+                        "The command to save the current canvas to file is  s filename: "
+                    ).split(),
+                )
+            )
+            if (type(x[0]) == str) and (x[0] == "S"):
+                try:
+                    filename = x[1]
+
+                    canvas = writeCanvas2File(canvas, filename)
+                    draw_menu()
+                except Exception:
+                    print("Invalid selection, please try again.\n")
+                    draw_menu()
+        except KeyError:
+            print("Invalid selection, please try again.\n")
+            draw_menu()
+
+    return canvas
 
 
 # Back to main menu
@@ -113,9 +241,19 @@ menu_actions = {
     "main_menu": main_menu,
     "1": menu1,
     "2": menu2,
-    "9": back,
-    "0": exit,
+    "3": menu3,
+    "4": menu4,
+    "5": menu5,
+    "q": exit,
 }
+exec_draw = {
+    "main_menu": main_menu,
+    "1": menu1,
+    "2": exit,
+    "9": back,
+    "q": exit,
+}
+
 
 # =======================
 #      MAIN PROGRAM
